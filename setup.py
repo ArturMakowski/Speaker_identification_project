39fff4989a3c34d2c2d9f9e546bf96394438d1dd
Artur Makowski <artmak1@st.amu.edu.pl>
2023-07-10 20:23:32 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Speaker identification with Pytorch',
+    author='Artur Makowski',
+    license='MIT',
+)
